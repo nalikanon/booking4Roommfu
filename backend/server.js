970e8261bf30b1fac6a79fd702cb33dfa128a5a2
TunDOTio <tuntun.pam3164@gmail.com>
2026-02-06 14:16:44 +0700

    l

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,12 @@ const API_HOST = (process.env.API_HOST || "https://roombooking.mfu.ac.th/apiroom
 app.use(cors());
 app.use(bodyParser.json());
 
+// Global Request Logger
+app.use((req, res, next) => {
+    console.log(`📨 [INCOMING] ${req.method} ${req.url}`);
+    next();
+});
+
 // Authentication Endpoint Proxy
 
 // OAuth2 Token Exchange
