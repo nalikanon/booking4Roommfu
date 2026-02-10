d2d1a2916ee00a9c94e35c68ad15b4282aaff4f8
TunDOTio <tuntun.pam3164@gmail.com>
2026-02-10 14:20:59 +0700

    q

--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -197,6 +197,7 @@ onMounted(() => {
               <button 
                 class="cancel-btn" 
                 @click="openCancelModal(item.guid)"
+                v-if="item.statusClass !== 'cancelled'"
               >
                 <span>✖</span> {{ t.cancelBooking || 'Cancel' }}
               </button>
