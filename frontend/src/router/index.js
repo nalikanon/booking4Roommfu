d1b3908166849f70f26d1637584d752b8b5dfb26
TunDOTio <tuntun.pam3164@gmail.com>
2026-03-13 11:09:11 +0700

    tab title

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,16 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 
 const routes = [
-    { path: '/', component: Login },
-    { path: '/auth/callback', component: () => import('../views/AuthCallback.vue') },
-    { path: '/select-room', component: () => import('../views/selectRoom.vue') },
-    { path: '/classroom-list', component: () => import('../views/ClassroomList.vue') },
-    { path: '/history', component: () => import('../views/History.vue') }
+    { path: '/', component: Login, meta: { title: 'Login' } },
+    { path: '/auth/callback', component: () => import('../views/AuthCallback.vue'), meta: { title: 'Loading' } },
+    { path: '/select-room', component: () => import('../views/selectRoom.vue'), meta: { title: 'Home' } },
+    { path: '/classroom-list', component: () => import('../views/ClassroomList.vue'), meta: { title: 'Room List' } },
+    { path: '/history', component: () => import('../views/History.vue'), meta: { title: 'Booking History' } }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.afterEach((to) => {
+    const title = to.meta?.title
+    document.title = title ? `${title} - Roombooking` : 'Roombooking'
 })
 
 export default router
