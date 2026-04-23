57b5ac8fd08a44ba049b90cded341a95b2521c63
TunDOTio <tuntun.pam3164@gmail.com>
2026-04-23 15:42:09 +0700

    Revert "Update probe_auth.js"
    This reverts commit 9a945d35460a4279b4ef3f4e47fa1bdb7db69fde.

--- backend/probe_auth.js
+++ backend/probe_auth.js
@@ -4,10 +4,10 @@ import https from 'https';
 import fs from 'fs';
 
 const DEFAULT_HOSTS = [
-    "https://192.168.11.74",
-    "https://192.168.11.74",
-    "https://192.168.11.74",
-    "https://192.168.11.74"
+    "https://roombooking.mfu.ac.th/apiroombooking",
+    "https://roombooking.mfu.ac.th/api",
+    "https://roombooking.mfu.ac.th",
+    "https://apitest.mfu.ac.th/apiroombooking"
 ];
 
 const HOSTS = process.env.API_HOSTS
